6efb9c5ce517d19ce4ddf3b9566cbc0dea3ff14b
Jeremy Prevost <jprevost@mit.edu>
2018-10-17 10:01:50 -0400

    scss

---
+++ lib/mitlibraries/theme.rb
@@ -0,0 +1,7 @@
+require 'mitlibraries/theme/version'
+
+module Mitlibraries
+  module Theme
+    # Your code goes here...
+  end
+end
---
+++ test/mitlibraries/theme_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class Mitlibraries::ThemeTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Mitlibraries::Theme::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
