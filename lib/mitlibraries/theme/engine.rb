dc4978e10fa90a10de3ef87310f5ea7949baac72
Matt Bernhardt <mjbernha@mit.edu>
2022-09-01 16:08:07 -0400

    Copy vendor, layouts, and helpers from Tesseract
    ** Why are these changes being introduced:
    
    * Starting from the boilerplate engine, we need to copy/paste the
      contents of the proof-of-concept gem (Tesseract) which was developed
      earlier in this ticket.
    * We also need to remove an unused application layout template in a
      namespaced folder path, to prevent future confusion.
    * The tests for the link helper class need to be added.
    * The theme assets in vendor/assets need to be added to the asset
      pipeline.
    
    ** Relevant ticket(s):
    
    * https://mitlibraries.atlassian.net/browse/engx-175
    
    ** How does this address that need:
    
    * This copies the key parts of the Tesseract proof of concept into this
      gem:
      * link helper class (including tests)
      * layout partials
      * vendor/assets materials (including adding them to asset pipeline)
    * The changes made in these files from the old theme gem will appear in
      the overall diff. Most of these changes are the addition of new
      boilerplate files, but the following specific changes should be noted:
      * The addition of `extend self` to the link helper class, which helps
        expose `nav_link_to` to implementing apps.
      * The addition of the csp_meta_tag directive to the application layout
      * The change of the body class from `app-thing` to `app-tesseract`
    * Removes the unused application layout template from its namespaced
      path. The file which gets used is still in the prior location.
    
    ** Document any side effects to this change:
    
    * None

--- lib/mitlibraries/theme/engine.rb
+++ lib/mitlibraries/theme/engine.rb
@@ -2,6 +2,10 @@ module Mitlibraries
   module Theme
     class Engine < ::Rails::Engine
       isolate_namespace Mitlibraries::Theme
+
+      initializer "mitlibraries.theme.assets.precompile" do |app|
+        app.config.assets.precompile += %w( favicon.ico mitlib-wordmark.svg vi-shape7-tp.svg libraries-main.css )
+      end
     end
   end
 end
